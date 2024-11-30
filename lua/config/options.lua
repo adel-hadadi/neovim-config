90b3c6b8af76a95a487e6088bd6e10b47a9f83c7
Adel Hadadi <hadadiadel81@gmail.com>
2024-11-30 16:38:37 +0330

    fix: leader key

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.mapleader = " "
+
 local opt = vim.opt
 
 opt.shiftwidth = 4
