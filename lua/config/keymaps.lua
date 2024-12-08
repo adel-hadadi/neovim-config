fc99d786ce71dc3353c41839f33446aa9b018c0b
Adel Hadadi <hadadiadel81@gmail.com>
2024-12-08 15:31:41 +0330

    fix: add silent otpion to keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-f>", ":Telescope find_files <CR>")
+vim.keymap.set("n", "<C-f>", ":Telescope find_files <CR>", { silent = true })
+vim.keymap.set("n", "<C-d>", ":DBUIToggle<CR>", { silent = true })
